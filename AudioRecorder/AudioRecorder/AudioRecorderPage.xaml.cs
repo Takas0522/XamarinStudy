c5049f946f953d4b1fbf4244d6c0925d91d296ca
ookawatakashi <t_o_19880522@outlook.com>
2017-02-13 00:12:09 +0900

    * App.xaml: 1stCommit "Recording"Function
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Recorder_iOS.cs:
    * packages.config:
    * packages.config:
    * MainActivity.cs:
    * Entitlements.plist:
    * App.xaml.cs:
    * LaunchScreen.storyboard:
    * AboutAssets.txt:
    * packages.config:
    * AssemblyInfo.cs:
    * styles.xml:
    * icon.png:
    * AboutResources.txt:
    * Tabbar.axml:
    * Toolbar.axml:
    * IRecorder.cs:
    * AudioRecorderPage.xaml:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * icon.png:
    * AudioRecorderPage.xaml.cs:
    * icon.png:
    * AssemblyInfo.cs:
    * icon.png:

---
+++ AudioRecorder/AudioRecorder/AudioRecorderPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AudioRecorder.Interface;
+using Xamarin.Forms;
+using PCLStorage;
+using System.IO;
+
+namespace AudioRecorder
+{
+	public partial class AudioRecorderPage : ContentPage
+	{
+		public AudioRecorderPage()
+		{
+			InitializeComponent();
+			StartButton.Clicked += new EventHandler(StartButtonClicked);
+			EndButton.Clicked += new EventHandler(EndButtonClicked);
+		}
+		private void StartButtonClicked(object sender, EventArgs e) {
+			var recService = DependencyService.Get<IRecorder>();
+			recService.RecStart();
+		}
+		private void EndButtonClicked(object sender, EventArgs e) {
+			var recService = DependencyService.Get<IRecorder>();
+			recService.RecEnd();
+		}
+	}
+}
---
+++ AudioRecorder/AudioRecorder/Interface/IRecorder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace AudioRecorder.Interface
+{
+	public interface IRecorder
+	{
+		void RecStart();
+		void RecEnd();
+	}
+}
