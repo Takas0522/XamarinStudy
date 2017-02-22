2bfec785fe756a3bd6fcb6eb800073e029577b30
ookawatakashi <t_o_19880522@outlook.com>
2017-02-23 08:51:29 +0900

    * AudioRecorder.userprefs: ?
    * AudioRecorderPage.xaml: ラベル追加
    
    * AudioRecorderPage.xaml.cs: Start/EndでButtonのEnableがChangeするようにー。

--- AudioRecorder/AudioRecorder/AudioRecorderPage.xaml.cs
+++ AudioRecorder/AudioRecorder/AudioRecorderPage.xaml.cs
@@ -11,14 +11,24 @@ namespace AudioRecorder
 		public AudioRecorderPage()
 		{
 			InitializeComponent();
+
+			StartButton.IsEnabled = true;
+			EndButton.IsEnabled = false;
+
 			StartButton.Clicked += new EventHandler(StartButtonClicked);
 			EndButton.Clicked += new EventHandler(EndButtonClicked);
 		}
 		private void StartButtonClicked(object sender, EventArgs e) {
+			StartButton.IsEnabled = false;
+			EndButton.IsEnabled = true;
+
 			var recService = DependencyService.Get<IRecorder>();
 			recService.RecStart();
 		}
 		private void EndButtonClicked(object sender, EventArgs e) {
+			StartButton.IsEnabled = true;
+			EndButton.IsEnabled = false;
+
 			var recService = DependencyService.Get<IRecorder>();
 			recService.RecEnd();
 		}
